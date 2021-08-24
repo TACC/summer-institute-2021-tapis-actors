4d4f67cac723ddd55e2af602005d89cc5aa5383d
Shweta Gopal <sgopal@sgopal-mbp19.local>
2021-08-24 12:50:14 -0500

    modifying email access from secrets.json

--- docs/scripts/email_actor/actor.py
+++ docs/scripts/email_actor/actor.py
@@ -3,14 +3,14 @@ from agavepy.actors import get_context
 import smtplib
 
 # function to email the message
-def email_message(m):
+def email_message(m, user_email, user_password):
     """Send Email from Actor With User Message"""
+
     context = get_context()
+
     if m == " ":
         print("Empty Message!")
     else:
-        user_email = context["USER_EMAIL"]
-        password = context["USER_PASSWORD"]
         sent_from = user_email
         to = ['shwetagopaul92@gmail.com']
         subject = 'Actor Email'
@@ -19,7 +19,7 @@ def email_message(m):
         try:
             smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             smtp_server.ehlo()
-            smtp_server.login(user_email, password)
+            smtp_server.login(user_email, user_password)
             smtp_server.sendmail(sent_from, to, message)
             smtp_server.close()
             print("Email sent successfully!")
@@ -31,8 +31,9 @@ def main():
     """Main entry to grab message context from user input"""
     context = get_context()
     message = context['raw_message']
-    print(context['USER_EMAIL'])
-    email_message(message)
+    user_email = context['USER_EMAIL']
+    user_password = context['USER_PASSWORD']
+    email_message(message, user_email, user_password)
 
 if __name__ == '__main__':
     main()
