c50fe8c27acd0d7155504b87c53a9f63cb088661
Shweta Gopal <sgopal@sgopal-mbp19.local>
2021-08-25 17:55:15 -0500

    modifying documentation

--- docs/index.rst
+++ docs/index.rst
@@ -1,4 +1,4 @@
-.. TACC Summer Instittute Tapis Actors documentation master file, created by
+.. TACC Summer Institute Tapis Actors documentation master file, created by
    sphinx-quickstart on Thu Jul 15 16:18:15 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
@@ -14,7 +14,7 @@ Welcome to TACC Summer Instittute Tapis Actors's documentation
    email-actor/00.overview
    slackbot-actor/00.overview
    upstream-messenger/00.deployupstream
-
+   cronjob-actor/00.cronjobactor
 
 Indices and tables
 ==================
--- docs/scripts/email_actor/actor.py
+++ docs/scripts/email_actor/actor.py
@@ -1,25 +1,29 @@
 """Send Email from Actor with User Message"""
 from agavepy.actors import get_context
 import smtplib
+import os
 
 # function to email the message
-def email_message(m, user_email, user_password):
+def email_message(m):
     """Send Email from Actor With User Message"""
 
-    context = get_context()
+    # grab the environment variables set
+    useremail_from = os.environ.get('useremail_from')
+    useremail_to = os.environ.get('useremail_to')
+    userpassword = os.environ.get('userpassword')
 
     if m == " ":
         print("Empty Message!")
     else:
-        sent_from = user_email
-        to = ['shwetagopaul92@gmail.com']
+        sent_from = useremail_from
+        to = [useremail_to]
         subject = 'Actor Email'
         message = m
 
         try:
             smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             smtp_server.ehlo()
-            smtp_server.login(user_email, user_password)
+            smtp_server.login(useremail_from, userpassword)
             smtp_server.sendmail(sent_from, to, message)
             smtp_server.close()
             print("Email sent successfully!")
@@ -31,9 +35,7 @@ def main():
     """Main entry to grab message context from user input"""
     context = get_context()
     message = context['raw_message']
-    user_email = context['USER_EMAIL']
-    user_password = context['USER_PASSWORD']
-    email_message(message, user_email, user_password)
+    email_message(message)
 
 if __name__ == '__main__':
     main()
