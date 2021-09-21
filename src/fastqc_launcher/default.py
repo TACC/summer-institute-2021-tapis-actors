8a302f3f55199cc4fae3bd0e5bb65719b44eeb03
Ethan Ho <eho@tacc.utexas.edu>
2021-09-21 16:52:22 -0500

    Debugging /work mount in upstream-messenger

--- src/fastqc_launcher/default.py
+++ src/fastqc_launcher/default.py
@@ -1,29 +1,27 @@
-
 import os
 from agavepy.actors import get_context, get_client
 
 
 def main():
-  context = get_context()
-  message = context['raw_message']
-  print("Actor received message: {}".format(message))
+   context = get_context()
+   fastq_path = context['raw_message']
+   print("Actor received message: {}".format(fastq_path))
 
-  # Get an active Tapis client
-  client = get_client()
+   # Get an active Tapis client
+   client = get_client()
 
-  # Using our Tapis client, submit a job to Tapis App eho-fastqc-0.11.9
-  body = {
-     "name": "fastqc-test",
-     "appId": "eho-fastqc-0.11.9",
-     "archive": False,
-     "inputs": {
-        "fastq": message
-     }
-  }
-  response = client.jobs.submit(body=body)
-  print(response)
-  print("Successfully submitted job '{}' to Tapis App '{}'".format(response['id'], response['appId']))
+   # Using our Tapis client, submit a job to Tapis App eho-fastqc-0.11.9
+   body = {
+      "name": "fastqc-test",
+      "appId": "eho-fastqc-0.11.9",
+      "archive": False,
+      "inputs": {
+         "fastq": "agave://eho.work.storage/{}".format(os.path.basename(fastq_path))
+      }
+   }
+   response = client.jobs.submit(body=body)
+   print("Successfully submitted job {} to Tapis App {}".format(response['id'], response['appId']))
 
 
 if __name__ == '__main__':
-   main()
+    main()
--- src/upstream_messenger/default.py
+++ src/upstream_messenger/default.py
@@ -3,15 +3,16 @@ from agavepy.actors import get_context, get_client
 import requests
 
 
-def download_fastq(out_path) -> None:
+def download_fastq(fastq_path) -> None:
+   """Download a demo fastq file from the web to path `fastq_path`."""
    url = 'https://raw.githubusercontent.com/eho-tacc/fastqc_app/main/tests/data_R1_001.fastq'
    fq_file = requests.get(url, allow_redirects=True)
-   with open(out_path, 'wb') as f:
+   with open(fastq_path, 'wb') as f:
       f.write(fq_file.content)
 
 
 def main():
-   """Main entry to grab message context from user input"""
+   """Main entrypoint"""
    context = get_context()
    m = context['raw_message']
    print("Actor received message: {}".format(m))
@@ -25,12 +26,15 @@ def main():
    # downstream_actor_id = os.environ['DOWNSTREAM_ACTOR_ID']
 
    # Download our fastq file to TACC Stockyard
-   out_path = '/work/06634/eho/my_reads.fastq'
-   download_fastq(out_path)
+   fastq_path = '/work/06634/eho/my_reads.fastq'
+   print(os.listdir('/'))
+   print(os.listdir('/mnt'))
+   print(os.listdir('/work/06634/eho/'))
+   download_fastq(fastq_path)
 
    # Using our Tapis client, send the message containing file path
    # to the downstream Actor
-   message = out_path
+   message = fastq_path
    print("Sending message '{}' to {}".format(message, downstream_actor_id))
    response = client.actors.sendMessage(actorId=downstream_actor_id, body={"message": message})
    print("Successfully triggered execution '{}' on actor '{}'".format(response['executionId'], downstream_actor_id))
